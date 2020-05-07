490983a1b582068e0c26eb4fb1344ec941b38c3d
Wei Guo <wei.parker.guo.sg@gmail.com>
2020-05-07 20:19:19 +0800

    hotfix: fixed the issue where mouse pos can't be registered

--- src/GUI/Canvas.java
+++ src/GUI/Canvas.java
@@ -13,6 +13,8 @@ public class Canvas extends JPanel {
     //attributes
     private int[] mouse_loc = {0,0};
     private boolean mouse_inside = false;
+    private boolean mouse_pressed = false;
+    private ArrayList<Point> pressed_points = new ArrayList<>();
 
     public Canvas(){
         super();
@@ -25,12 +27,12 @@ public class Canvas extends JPanel {
 
             @Override
             public void mousePressed(MouseEvent e) {
-
+                mouse_pressed = true;
             }
 
             @Override
             public void mouseReleased(MouseEvent e) {
-
+                mouse_pressed = false;
             }
 
             @Override
@@ -50,6 +52,10 @@ public class Canvas extends JPanel {
         super.paintComponent(g);
         construct(g);
         update_mouse_activity(g);
+        for(Point p : pressed_points){
+            g.setColor(Color.orange);
+            g.fillRect(p.x + 1, p.y + 1, 18, 18);
+        }
     }
 
     //initiate interface
@@ -57,6 +63,8 @@ public class Canvas extends JPanel {
         //fill background
         int width = getWidth();
         int height = getHeight();
+        g.setColor(COLOR.ideal_dark_opaque);
+        g.fillRect(0,0, width/20*20, height/20*20);
         g.setColor(COLOR.ideal_dark_highlight_opaque);
         for(int i = 0; i < width/20; i++){
             for(int j = 0; j < height/20; j++){
@@ -81,6 +89,11 @@ public class Canvas extends JPanel {
 
             g.setColor(Color.orange);
             g.fillRect(mouse_loc[0]+1,mouse_loc[1]+1,18,18);
+
+            if(mouse_pressed){
+                Point new_point = new Point(mouse_loc[0], mouse_loc[1]);
+                pressed_points.add(new_point);
+            }
         }
 
     }
